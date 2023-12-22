011b8f0ac02721b631fc12bc231a8197399c0a17
alessandro dandea <alessandromanueldandea@gmail.com>
2023-12-22 08:45:36 +0100

    v 0.1.0 default

---
+++ src/components/Main.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+function Main(){
+    return(
+        <div>
+            <h1>Fun Facts about React</h1>
+            <ul>
+                <li>Born in 2013</li>
+                <li>created by jordan walke</li>
+                <li>100k stars on github</li>
+                <li>maintained by facebook</li>
+            </ul>
+        </div>
+    )
+}
+export default Main
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import Reactlogo from '../img/logo192.png'
+
+function Navbar() {
+    return (
+        <div>
+            <img src={Reactlogo}></img>
+            <h1>React Logo</h1>
+            <h2>React course- pg1</h2>
+        </div>
+    )
+}
+export default Navbar
